aba39445603c6121763d7c8bca7abf9686fcd5ed
arkachatterjee857-oss <arkachatterjee857@gmail.com>
2026-08-29 22:56:56 +0530

    Update instruction register with IR load control

--- rtl/instruction_register.v
+++ rtl/instruction_register.v
@@ -1,15 +1,22 @@
 module instruction_register(
+
     input clk,
     input rst,
+    input ir_load,
     input [7:0] instruction_in,
+
     output reg [7:0] instruction_out
+
 );
 
 always @(posedge clk or posedge rst) begin
-    if (rst)
+
+    if(rst)
         instruction_out <= 8'd0;
-    else
+
+    else if (ir_load)
         instruction_out <= instruction_in;
+
 end
 
 endmodule
