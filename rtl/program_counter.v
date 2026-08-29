a6d4480c7bfa087899620016b967bacd88928e2e
arkachatterjee857-oss <arkachatterjee857@gmail.com>
2026-08-29 22:56:46 +0530

    Update program counter with controlled increment

--- rtl/program_counter.v
+++ rtl/program_counter.v
@@ -1,14 +1,20 @@
 module program_counter(
+
     input clk,
     input rst,
+    input pc_increment,
     output reg [7:0] pc
+
 );
 
 always @(posedge clk or posedge rst) begin
-    if (rst)
+
+    if(rst)
         pc <= 8'd0;
-    else
+
+    else if(pc_increment)
         pc <= pc + 1'b1;
+
 end
 
 endmodule
