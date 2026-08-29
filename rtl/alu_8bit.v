c2caf2bb76e776bd4c077157a4a5401f7215b31f
arkachatterjee857-oss <arkachatterjee857@gmail.com>
2026-08-29 22:56:33 +0530

    Update ALU to latest 4-bit opcode and MOV version

--- rtl/alu_8bit.v
+++ rtl/alu_8bit.v
@@ -1,51 +1,83 @@
+`timescale 1ns / 1ps
+
 module alu_8bit(
+
     input [7:0] A,
     input [7:0] B,
-    input [2:0] opcode,
+    input [3:0] opcode,
+
     output reg [7:0] result,
     output reg carry,
     output reg overflow,
     output reg zero,
     output reg negative
-);
 
+);
 reg [8:0] temp;
 
 always @(*) begin
+
     result   = 8'd0;
     carry    = 1'b0;
     overflow = 1'b0;
-    temp     = 9'd0;
 
     case(opcode)
-        3'b000: begin
+
+        4'b0000: begin
             temp   = A + B;
             result = temp[7:0];
             carry  = temp[8];
+            overflow = (~(A[7] ^ B[7])) & (result[7] ^ A[7]);
         end
-        3'b001: begin
+
+        4'b0001: begin
             temp   = A - B;
             result = temp[7:0];
             carry  = temp[8];
+            overflow = ((A[7] ^ B[7])) & (result[7] ^ A[7]);
         end
-        3'b010: result = A & B;
-        3'b011: result = A | B;
-        3'b100: result = A ^ B;
-        3'b101: result = ~A;
-        3'b110: begin
-            temp   = A + 1'b1;
+
+        4'b0010: result = A & B;
+
+        4'b0011: result = A | B;
+
+        4'b0100: result = A ^ B;
+
+        4'b0101: result = ~A;
+
+        4'b0110: begin
+            temp   = A + 1;
             result = temp[7:0];
             carry  = temp[8];
+            overflow = (~(A[7] ^ B[7])) & (result[7] ^ A[7]);
         end
-        3'b111: begin
-            temp   = A - 1'b1;
+
+        4'b0111: begin
+            temp   = A - 1;
             result = temp[7:0];
             carry  = temp[8];
+            overflow = ((A[7] ^ B[7])) & (result[7] ^ A[7]);
+        end
+        
+        4'b1000: begin // left shift
+            temp=A<<2;
+            result=temp[7:0];
+        end
+        
+        4'b1001: begin // right shift
+            temp=A>>2;
+            result=temp[7:0];
+        end
+        
+        4'b1010: begin // MOV function
+            temp=B[7:0];
+            result=temp[7:0];
         end
     endcase
 
     zero     = (result == 8'd0);
     negative = result[7];
+
 end
 
 endmodule
