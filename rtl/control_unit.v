3ba185ac0df4ca56045634d01cd0e5563b7d4282
arkachatterjee857-oss <arkachatterjee857@gmail.com>
2026-08-29 22:57:02 +0530

    Update control unit to latest multicycle HALT/control version

--- rtl/control_unit.v
+++ rtl/control_unit.v
@@ -1,41 +1,84 @@
 module control_unit(
+
     input clk,
     input rst,
+
     input [7:0] instruction,
-    output reg [2:0] alu_opcode,
-    output reg reg_write
+
+    output reg [3:0] alu_opcode,
+    output reg reg_write,
+    output reg halt,
+    output reg pc_increment,
+    output reg ir_load
+
+
 );
 
-localparam FETCH     = 2'b00;
-localparam DECODE    = 2'b01;
-localparam EXECUTE   = 2'b10;
-localparam WRITEBACK = 2'b11;
+parameter FETCH     = 2'b00;
+parameter DECODE    = 2'b01;
+parameter EXECUTE   = 2'b10;
+parameter WRITEBACK = 2'b11;
 
 reg [1:0] state;
 reg [1:0] next_state;
 
 always @(posedge clk or posedge rst) begin
-    if (rst)
+
+    if(rst)
         state <= FETCH;
     else
         state <= next_state;
+
 end
 
 always @(*) begin
-    alu_opcode = instruction[7:5];
-    reg_write  = 1'b0;
-    next_state = FETCH;
 
-    case (state)
-        FETCH:     next_state = DECODE;
-        DECODE:    next_state = EXECUTE;
-        EXECUTE:   next_state = WRITEBACK;
-        WRITEBACK: begin
-            reg_write  = 1'b1;
-            next_state = FETCH;
+    alu_opcode = instruction[7:4];
+    reg_write  = 1'b0;
+    halt = 1'b0;
+
+    case(state)
+
+        FETCH:
+        begin
+            next_state = DECODE;
+            pc_increment = 1'b1;
+            ir_load=1;
         end
-        default:   next_state = FETCH;
+
+        DECODE: begin
+            
+            pc_increment = 1'b0;
+            ir_load=0;
+            if (alu_opcode==4'b1111)
+            begin
+                halt=1;
+            end
+            else
+            begin
+                next_state = EXECUTE;
+                halt=0;
+            end
+        end
+
+        EXECUTE:begin
+            next_state = WRITEBACK;
+            pc_increment = 1'b0;
+            ir_load=0;
+        end
+
+        WRITEBACK: begin
+            reg_write = 1'b1;
+            next_state = FETCH;
+            pc_increment = 1'b0;
+            ir_load=0;
+        end
+
+        default:
+            next_state = FETCH;
+
     endcase
+
 end
 
 endmodule
